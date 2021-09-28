a7b4b5ca7de69468b3e14279674adb4a593c71f9
Rick Arnett <rick.arnett@gmail.com>
2021-09-28 18:16:09 -0400

    Better flow control

--- sfdx-cicd.sh
+++ sfdx-cicd.sh
@@ -1,16 +1,23 @@
+#!/bin/bash
+set -o errexit -o pipefail -o noclobber -o nounset
+
 # Download sfdx binary and setup
 wget -q https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
 mkdir ~/sfdx-cli
 tar xJf sfdx-linux-x64.tar.xz -C ~/sfdx-cli --strip-components 1
 export PATH=~/sfdx-cli/bin:$PATH
 
-# Instal sfpowerkit
+# Install sfpowerkit
 echo 'y' | sfdx plugins:install sfpowerkit
 
+VALIDATE_ONLY=`echo $VALIDATE_ONLY | tr '[:upper:]' '[:lower:]'`
 if [[ $VALIDATE_ONLY = true ]]; then
   VALIDATE_FLAG='-c';
-else
+elif [[ $VALIDATE_ONLY = false ]]
   VALIDATE_FLAG='';
+else
+  echo "Bad Validate param...choose true or false"
+  exit 2
 fi
 
 echo "Validate flag"
@@ -25,4 +32,4 @@ rm sfdx_auth.txt
 sfdx sfpowerkit:project:diff -r $REVISION_FROM -t $REVISION_TO -d diffdeploy
 
 # Deploy diff
-sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal -c --apiversion=$API_VERSION
+sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal $VALIDATE_FLAG --apiversion=$API_VERSION
