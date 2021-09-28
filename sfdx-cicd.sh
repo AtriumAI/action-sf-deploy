20a0f2dd1f950de582c661d6abcf65f8074afa49
Rick Arnett <rick.arnett@gmail.com>
2021-09-28 17:36:44 -0400

    Pass env to bash script

--- sfdx-cicd.sh
+++ sfdx-cicd.sh
@@ -13,9 +13,9 @@ echo 'y' | sfdx plugins:install sfpowerkit
 
 # Echo test
 echo "Input validate only"
-echo $INPUT_VALIDATE_ONLY
+echo $VALIDATE_ONLY
 
-if [[ $INPUT_VALIDATE_ONLY = true ]]; then
+if [[ $VALIDATE_ONLY = true ]]; then
   VALIDATE_FLAG='-c';
 else
   VALIDATE_FLAG='';
@@ -24,13 +24,13 @@ fi
 echo "Validate flag"
 echo $VALIDATE_FLAG
 
-echo $INPUT_SFDX-AUTH-URL > sfdx_auth.txt
+echo $SFDX-AUTH-URL > sfdx_auth.txt
 cat sfdx_auth.txt
 
 sfdx force:auth:sfdxurl:store -f sfdx_auth.txt -s -a SFOrg
 
 rm sfdx_auth.txt
 
-sfdx sfpowerkit:project:diff -r $INPUT_REVISION-FROM -t $INPUT_REVISION-TO -d diffdeploy
+sfdx sfpowerkit:project:diff -r $REVISION-FROM -t $REVISION-TO -d diffdeploy
 
-sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal -c --apiversion=$INPUT_API-VERION
+sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal -c --apiversion=$API-VERSION
