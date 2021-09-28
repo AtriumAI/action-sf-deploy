cc1627dc313239a922bc62e73174644378367182
Rick Arnett <rick.arnett@gmail.com>
2021-09-28 17:49:31 -0400

    Clean up script

--- sfdx-cicd.sh
+++ sfdx-cicd.sh
@@ -2,18 +2,10 @@
 wget -q https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
 mkdir ~/sfdx-cli
 tar xJf sfdx-linux-x64.tar.xz -C ~/sfdx-cli --strip-components 1
-echo "Path 1"
-echo $PATH
 export PATH=~/sfdx-cli/bin:$PATH
-echo "Path 2"
-echo $PATH
 
 # Instal sfpowerkit
 echo 'y' | sfdx plugins:install sfpowerkit
-
-# Echo test
-echo "Input validate only"
-echo $VALIDATE_ONLY
 
 if [[ $VALIDATE_ONLY = true ]]; then
   VALIDATE_FLAG='-c';
@@ -24,13 +16,13 @@ fi
 echo "Validate flag"
 echo $VALIDATE_FLAG
 
+# Auth sfdx into org with auth url
 echo $SFDX_AUTH_URL > sfdx_auth.txt
-cat sfdx_auth.txt
-
 sfdx force:auth:sfdxurl:store -f sfdx_auth.txt -s -a SFOrg
-
 rm sfdx_auth.txt
 
+# Prepare diff
 sfdx sfpowerkit:project:diff -r $REVISION_FROM -t $REVISION_TO -d diffdeploy
 
+# Deploy diff
 sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal -c --apiversion=$API_VERSION
