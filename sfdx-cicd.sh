2cb7ab03ed808b38c30eaa705ef89b819ce9b4aa
“ethan-atrium-ai” <ethan@atrium.ai>
2022-01-28 08:26:15 -0600

    #1-Allow---testlevel-and--runtests-as-optional-parameters

--- sfdx-cicd.sh
+++ sfdx-cicd.sh
@@ -10,7 +10,9 @@ export PATH=~/sfdx-cli/bin:$PATH
 # Install sfpowerkit
 echo 'y' | sfdx plugins:install sfpowerkit
 
+
 VALIDATE_ONLY=`echo $VALIDATE_ONLY | tr '[:upper:]' '[:lower:]'`
+echo $VALIDATE_ONLY
 if [[ $VALIDATE_ONLY = true ]]; then
   VALIDATE_FLAG='-c';
 elif [[ $VALIDATE_ONLY = false ]]; then
@@ -20,13 +22,35 @@ else
   exit 2
 fi
 
+TEST_LEVEL=`echo $TEST_LEVEL | tr '[:upper:]' '[:lower:]'`
+if [[ $TEST_LEVEL = 'runspecifiedtests' ]]; then
+  TEST_LEVEL='--testlevel RunSpecifiedTests';
+elif [[ $TEST_LEVEL = 'runlocaltests' ]]; then
+  TEST_LEVEL='--testlevel RunLocalTests';
+else 
+  TEST_LEVEL='';
+  echo "Setting testlevel to run RunNoTests"
+fi
+
+SPECIFIED_TESTS=`echo $SPECIFIED_TESTS | tr -d ' '`
+echo 'Test Level: ' $TEST_LEVEL 'specificTest: ' $SPECIFIED_TESTS
+if [[ $TEST_LEVEL = '--testlevel RunSpecifiedTests' ]]; then
+  SPECIFIED_TESTS="--runtests $SPECIFIED_TESTS";
+else
+  SPECIFIED_TESTS='';
+fi
+
+echo 'Test Level: ' $TEST_LEVEL 'specificTest: ' $SPECIFIED_TESTS
+
 # Auth sfdx into org with auth url
 echo $SFDX_AUTH_URL > sfdx_auth.txt
 sfdx force:auth:sfdxurl:store -f sfdx_auth.txt -s -a SFOrg
 rm sfdx_auth.txt
 
+echo "sfdx diff command: sfdx sfpowerkit:project:diff -r $REVISION_FROM -t $REVISION_TO -d diffdeploy"
 # Prepare diff
 sfdx sfpowerkit:project:diff -r $REVISION_FROM -t $REVISION_TO -d diffdeploy
 
+echo "sfdx deploy command: sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal $VALIDATE_FLAG $TEST_LEVEL $SPECIFIED_TESTS --apiversion=$API_VERSION"
 # Deploy diff
-sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal $VALIDATE_FLAG --apiversion=$API_VERSION
+sfdx force:source:deploy -p diffdeploy/force-app -u SFOrg --json --loglevel fatal $VALIDATE_FLAG $TEST_LEVEL $SPECIFIED_TESTS --apiversion=$API_VERSION
